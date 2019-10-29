b5ea55df01c292ff14c94f736ec5172d76d3bc14
Elijah Baird <elijah_baird84@yahoo.com>
2019-10-28 18:47:26 -0700

    Fresh project

---
+++ Hades/Hades/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hades
+//
+//  Created by Elijah Baird on 10/28/19.
+//  Copyright © 2019 Elijah Baird. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
