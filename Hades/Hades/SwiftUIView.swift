e424a4dee4de43a005b99352348a22d1b653e2ba
Jeremy Shaw <jjsjeremy61@>
2019-12-02 19:49:54 -0800

    Did a storyboard, need to configure in swiftui

---
+++ Hades/Hades/SwiftUIView.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftUIView.swift
+//  Hades
+//
+//  Created by Jeremy Shaw on 12/2/19.
+//  Copyright © 2019 Elijah Baird. All rights reserved.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
